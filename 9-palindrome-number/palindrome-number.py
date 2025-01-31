aba4201cc570ad3e3c0bb84e2ce041b661ed9e33
h-aya1 <123396940+h-aya1@users.noreply.github.com>
2025-01-31 17:47:58 +0300

    Time: 9 ms (62.75%) | Memory: 12.3 MB (79.53%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        
+        s = str(x)  # Convert number to string
+        left, right = 0, len(s) - 1
+        
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        
+        return True
