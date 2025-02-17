351b18b354df2945e0207684e92870cac14050d8
h-aya1 <123396940+h-aya1@users.noreply.github.com>
2025-02-17 09:37:32 +0300

    Time: 633 ms (55.59%) | Memory: 26 MB (25.79%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums) <= 1:
+            return nums  
+
+        mid = len(nums) // 2
+        left = self.sortArray(nums[:mid])  
+        right = self.sortArray(nums[mid:]) 
+
+        sorted_arr = []
+        i = j = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                sorted_arr.append(left[i])
+                i += 1
+            else:
+                sorted_arr.append(right[j])
+                j += 1
+
+        sorted_arr.extend(left[i:])
+        sorted_arr.extend(right[j:])
+
+        return sorted_arr
